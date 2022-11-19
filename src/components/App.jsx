7e6c7dfa1e39af4e6a32098b4ed41b259c4f5890
Jake Brunner <Jbbrunner10@gmail.com>
2022-11-18 20:37:45 -0800

    basic setup

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import TradingViewWidget from 'react-tradingview-widget';
+
+
+function App() {
+  return (
+    <div className="App">  
+    <Header/>
+      <h1>hello world</h1>
+      
+    <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AutoGraphIcon from '@mui/icons-material/AutoGraph';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <AutoGraphIcon />
+        Paperboy Algo
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
